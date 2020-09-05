97b8219c34fedcee9d7f0f705b721b12d6ce2ff3
quyettvq <quyettvq@gmail.com>
2020-09-05 16:24:50 +0700

    update index.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+/**
+ * @type {I18nZone}
+ */
+const i18n = require('./dist/lib.min.js');
 
 /**
  *
  * @type {I18nZone}
  */
-module.exports = require('./dist/lib.min.js');
+module.exports = i18n;
